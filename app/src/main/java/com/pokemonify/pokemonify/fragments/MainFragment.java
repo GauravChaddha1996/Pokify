aee4505b70183758cb6f9a01cdb6ddeb4ac67246
GauravChaddha1996 <chaddhag@gmail.com>
2016-07-24 20:56:47 +0530

    Update Current Pokemon Fragment

--- app/src/main/java/com/pokemonify/pokemonify/fragments/MainFragment.java
+++ app/src/main/java/com/pokemonify/pokemonify/fragments/MainFragment.java
@@ -1,10 +1,13 @@
 package com.pokemonify.pokemonify.fragments;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.pokemonify.pokemonify.R;
@@ -14,7 +17,14 @@ import com.pokemonify.pokemonify.pokemondatabase.PokemonDto;
 
 public class MainFragment extends Fragment {
 
-    TextView mMyPokemon;
+    TextView mMyPokemonName;
+    TextView mMyPokemonHp;
+    TextView mMyPokemonType;
+    TextView mMyPokemonLevel;
+    TextView mMyPokemonWeight;
+    TextView mMyPokemonHeight;
+    ImageView mMyPokemonImage;
+
     PokemonDto currentMyPokemon;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,9 +35,23 @@ public class MainFragment extends Fragment {
     }
 
     private void initViews(View v){
-        mMyPokemon= (TextView) v.findViewById(R.id.myPokemon);
+        mMyPokemonName = (TextView) v.findViewById(R.id.myPokemonName);
+        mMyPokemonHp = (TextView) v.findViewById(R.id.myPokemonHp);
+        mMyPokemonType = (TextView) v.findViewById(R.id.myPokemonType);
+        mMyPokemonLevel = (TextView) v.findViewById(R.id.myPokemonLevel);
+        mMyPokemonWeight = (TextView) v.findViewById(R.id.myPokemonWeight);
+        mMyPokemonHeight = (TextView) v.findViewById(R.id.myPokemonHeight);
+        mMyPokemonImage = (ImageView) v.findViewById(R.id.myPokemonImage);
         setMyPokemon();
-        mMyPokemon.setText(currentMyPokemon.getName());
+        mMyPokemonName.setText(currentMyPokemon.getName());
+        mMyPokemonHp.setText(currentMyPokemon.getHp()+"Hp");
+        mMyPokemonType.setText(currentMyPokemon.getType());
+        mMyPokemonLevel.setText("Lvl "+currentMyPokemon.getLevel());
+        mMyPokemonWeight.setText(currentMyPokemon.getWeight()+"lbs");
+        mMyPokemonHeight.setText(currentMyPokemon.getHeight()+"cm");
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),getResources()
+                .getIdentifier(currentMyPokemon.getImagePath(), "drawable",getActivity().getPackageName()));
+        mMyPokemonImage.setImageBitmap(bitmap);
     }
 
     public void setMyPokemon() {
