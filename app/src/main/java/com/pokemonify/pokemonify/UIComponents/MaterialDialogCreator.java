b81ed1f267349757fe0ad4a79ebbd8ebaf25a5c3
GauravChaddha1996 <chaddhag@gmail.com>
2016-07-29 22:36:07 +0530

    Add edit features in pokemon edit

--- app/src/main/java/com/pokemonify/pokemonify/UIComponents/MaterialDialogCreator.java
+++ app/src/main/java/com/pokemonify/pokemonify/UIComponents/MaterialDialogCreator.java
@@ -3,8 +3,12 @@ package com.pokemonify.pokemonify.UIComponents;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.text.InputFilter;
+import android.text.InputType;
 import android.view.View;
 import android.widget.EditText;
+
+import com.pokemonify.pokemonify.R;
 
 /**
  * Created by gaurav on 28/7/16.
@@ -25,14 +29,49 @@ public class MaterialDialogCreator implements View.OnClickListener {
         if (shouldEdit) {
             AlertDialog.Builder alert = new AlertDialog.Builder(mContext);
             final EditText edittext = new EditText(mContext);
-            alert.setTitle("Enter your pokemon name");
+            switch (view.getId()) {
+                case R.id.pokemon_name:
+                    alert.setTitle("Enter your pokemon name");
+                    edittext.setFilters(new InputFilter[]{new InputFilter.LengthFilter(30)});
+                    break;
+                case R.id.pokemon_hp:
+                    alert.setTitle("Enter your pokemon hp");
+                    edittext.setInputType(InputType.TYPE_CLASS_NUMBER);
+                    edittext.setFilters(new InputFilter[]{new InputFilter.LengthFilter(8)});
+                    break;
+                case R.id.pokemon_type:
+                    alert.setTitle("Enter your pokemon type");
+                    edittext.setFilters(new InputFilter[]{new InputFilter.LengthFilter(30)});
+                    break;
+                case R.id.pokemon_weight:
+                    alert.setTitle("Enter your pokemon weight");
+                    edittext.setInputType(InputType.TYPE_CLASS_NUMBER);
+                    edittext.setFilters(new InputFilter[]{new InputFilter.LengthFilter(8)});
+                    break;
+                case R.id.pokemon_height:
+                    alert.setTitle("Enter your pokemon height");
+                    edittext.setInputType(InputType.TYPE_CLASS_NUMBER);
+                    edittext.setFilters(new InputFilter[]{new InputFilter.LengthFilter(8)});
+                    break;
+                case R.id.pokemon_desc:
+                    alert.setTitle("Enter your pokemon description");
+                    edittext.setSingleLine(false);
+                    edittext.setFilters(new InputFilter[]{new InputFilter.LengthFilter(120)});
+                    break;
+                case R.id.pokemon_level:
+                    alert.setTitle("Enter your pokemon level");
+                    edittext.setInputType(InputType.TYPE_CLASS_NUMBER);
+                    edittext.setFilters(new InputFilter[]{new InputFilter.LengthFilter(8)});
+                    break;
+            }
             alert.setView(edittext);
             alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int whichButton) {
-                    mOnClickCallBack.onPress(view, edittext.getText().toString());
+                    if (!edittext.getText().toString().isEmpty()) {
+                        mOnClickCallBack.onPress(view, edittext.getText().toString());
+                    }
                 }
             });
-
             alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int whichButton) {
 
